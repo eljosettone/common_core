e42a634721b53e2bc6aa6a14d7a2a3ec6b1fb3db
Fanny ALLEGRE <fannya2212@gmail.com>
2026-04-26 20:03:43 +0200

    libft

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,13 +6,13 @@
 /*   By: fannyallegre <fannyallegre@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 14:54:50 by fanalleg          #+#    #+#             */
-/*   Updated: 2026/04/26 19:58:16 by fannyallegr      ###   ########.fr       */
+/*   Updated: 2026/04/26 20:03:38 by fannyallegr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	free_list(t_list *lst, void (*del)(void *))
+static void	free_list(char *list, void (*del)(void *))
 {
 	t_list	*tmp;
 
